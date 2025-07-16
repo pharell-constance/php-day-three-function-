41d7d1011bc476b88a3d12499a9675596e847b89
æth3rboy <pharellc.dev@gmail.com>
2025-07-16 16:20:01 +0200

    new

--- exo_3.php
+++ exo_3.php
@@ -4,14 +4,14 @@ function calculImpot($revenu){
     if ($revenu >9999 && $revenu < 20000){
         echo ($revenu - 10000) * 0.10;
     }   else if ($revenu >20000 && $revenu < 35000){
-        echo ($revenu - 20000) * 0.18;
-    }   else if ($revenu >35000 && $revenu < 49999){
-        echo ($revenu - 35000) * 0.25;
+        echo $revenu * 0.18;
+    }   else if ($revenu >35000 && $revenu < 50000){
+        echo $revenu * 0.25;
     }   else if ($revenu > 50000){
-        echo ($revenu - 50000)* 0.35;
+        echo $revenu * 0.35;
     }   else {
         echo "0 %";
     }
 }
 
-calculImpot(64000);
+calculImpot(7000);
